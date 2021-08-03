75d07ca9bff2fad699af65a9b1781a6d245b1bec
miu <84560163+miu-hub@users.noreply.github.com>
2021-08-03 21:39:00 +0800

    '在之前的基础上完善了路由导航栏，刚开始了未登录状态的跳转'

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="index_box">
-    <p>欢迎使用头条管理系统</p>
+    <div id="main">
+      <p>欢迎使用头条管理系统</p>
+    </div>
   </div>
 </template>
 
@@ -12,13 +14,22 @@ export default {
 
 <style lang="less" scope>
 .index_box {
+  padding: 5% 5%;
   height: 100%;
   background-color: #000;
   background-image: url("../index/index_bg.jpg");
-}
 
-p {
-  font-size: 40px;
-  font-weight: 700;
+  #main {
+    width: 100%;
+    height: 80%;
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 10px 10px 5px #666;
+    p {
+      font-size: 40px;
+      font-weight: 700;
+      text-align: center;
+    }
+  }
 }
 </style>
--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -9,11 +9,19 @@
       </div>
 
       <!-- 分类组件 -->
+      <!-- 遍历导航路由 -->
       <ul>
-        <li v-for="(item, i) in to" :key="i" @click="updata_style(i)">
-          <router-link :to="'/' + to[i]"
-            ><i :class="['iconfont', 'icon-' + class_icon[i]]"></i>&nbsp;{{
-              text[i]
+        <!-- 绑定点击触发的样式类 -->
+        <!-- 使用绑定样式类来导入字体图标和文字/路由路径 -->
+        <li
+          :class="{ li_click: item.isclick }"
+          v-for="(item, i) in rout"
+          :key="i"
+          @click="updata_style(i)"
+        >
+          <router-link :to="'/' + item.to"
+            ><i :class="['iconfont', 'icon-' + item.class_icon]"></i>&nbsp;{{
+              item.text
             }}</router-link
           >
         </li>
@@ -35,7 +43,8 @@
         <div id="user_info">
           <div id="head_port"></div>
           &nbsp;&nbsp;
-          <p>未登录</p>
+          <!-- 跳转方法 -->
+          <p @click="push">未登录</p>
           &nbsp;&nbsp;
           <i class="iconfont icon-linecar105"></i>
         </div>
@@ -52,32 +61,76 @@
 <script>
 export default {
   name: "layout",
+
   data() {
     return {
-      to: ["", "conest", "matter", "issue", "comment", "fans", "user"],
-      class_icon: [
-        "shouye",
-        "rizhimingxi",
-        "icon_huabanfuben",
-        "tubiao-",
-        "pinglun",
-        "weibiaoti-",
-        "shezhi",
-      ],
-      text: [
-        "首页",
-        "内容管理",
-        "素材管理",
-        "发布文章",
-        "评论管理",
-        "粉丝管理",
-        "个人设置",
+      // 导航栏的详细配置
+      // to：路由路径
+      // class_icon---所需的字体图标
+      // text----展示的文字内容
+      // isclick----决定点击后li的样式
+      rout: [
+        { to: "", class_icon: "shouye", text: "首页", isclick: false },
+        {
+          to: "conest",
+          class_icon: "rizhimingxi",
+          text: "内容管理",
+          isclick: false,
+        },
+        {
+          to: "matter",
+          class_icon: "icon_huabanfuben",
+          text: "素材管理",
+          isclick: false,
+        },
+        {
+          to: "issue",
+          class_icon: "tubiao-",
+          text: "发布文章",
+          isclick: false,
+        },
+        {
+          to: "comment",
+          class_icon: "pinglun",
+          text: "评论管理",
+          isclick: false,
+        },
+        {
+          to: "fans",
+          class_icon: "weibiaoti-",
+          text: "粉丝管理",
+          isclick: false,
+        },
+        {
+          to: "user",
+          class_icon: "shezhi",
+          text: "个人设置",
+          isclick: false,
+        },
       ],
     };
   },
+
   methods: {
+    // 鼠标点击路由导航
     updata_style(i) {
-      console.log(i);
+      //i------获取的点击到元素的序号
+      const rout = this.rout;
+
+      // 使用foreach方法来遍历路由信息-----利用排他案例来改变除点击元素的样式
+      rout.forEach((item) => {
+        item.isclick = false;
+      });
+
+      // 点击的元素的样式状态改变
+      rout[i].isclick = true;
+    },
+
+    // 跳转方法----在未登录状态下跳转至登录页
+    push() {
+      this.$router.push({
+        name: "login",
+      });
     },
   },
 };
@@ -130,7 +183,7 @@ export default {
       margin-top: 10px;
       width: 100%;
       height: 90%;
-
+      // 路由导航
       li {
         width: 100%;
         height: 10%;
@@ -144,12 +197,19 @@ export default {
           height: 100%;
           color: #fff;
         }
-
+        // 鼠标经过
         &:hover {
-          background-color: rgba(0, 0, 0, 0.3);
           a {
             color: yellow;
           }
+        }
+      }
+
+      // 路由点击的样式
+      .li_click {
+        background-color: rgba(0, 0, 0, 0.3);
+        a {
+          color: red;
         }
       }
     }
@@ -206,6 +266,8 @@ export default {
           width: 50px;
           height: 50px;
           cursor: pointer;
+          border: 1px solid #666;
+          border-radius: 50%;
           background-color: #666;
           background-image: url("../index/user_header.jpg");
           background-repeat: no-repeat;
