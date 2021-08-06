871bb934eb55b1a35fa59b0cadc38558c4612a70
miu <84560163+miu-hub@users.noreply.github.com>
2021-08-06 18:24:41 +0800

    '完成了文章所有的请求模块,优化了部分体验(loader,时间筛选,频道筛选.....)'

--- src/apis/article.js
+++ src/apis/article.js
@@ -5,7 +5,7 @@
 // 获取请求模块
 import request from '../utils/request'
 // 暴露获取文章信息接口
-export const articles = (token, params) => {
+export const articles = (token, params, channel_id, begin_pubdate, end_pubdate) => {
 
     return request({
         // 请求方式
@@ -20,7 +20,25 @@ export const articles = (token, params) => {
 
         // params----get方法的配置要求------可传递给服务器
         params,
+        channel_id,
+        begin_pubdate,
+        end_pubdate,
     })
 
 
+};
+
+// 获取文章频道接口
+export const channel = (token) => {
+    return request({
+        methods: 'GET',
+        url: '/mp/v1_0/channels',
+
+        headers: {
+            // 用户令牌
+            Authorization: `Bearer ${token}`
+        },
+    })
 }
+
+
--- src/components/conest/conest.vue
+++ src/components/conest/conest.vue
@@ -69,17 +69,19 @@
             <!-- 频道设置 -->
             <p>频道：</p>
             <form>
-              <select name="bottom_munes" id="bottom_munes">
-                <option value="JavaScript">JavaScript</option>
-                <option value="jquery">Jquery</option>
-                <option value="Node.js">Node.js</option>
-                <option value="Html">Html</option>
-                <option value="Vue" selected>Vue</option>
-                <option value="Webpack">Webpack</option>
-                <option value="Ajax">Ajax</option>
-                <option value="Css">Css</option>
-                <option value="Git">Git</option>
-                <option value="GitHub">GitHub</option>
+              <!-- 绑定频道id -->
+              <select
+                v-model="channl_status"
+                name="bottom_munes"
+                id="bottom_munes"
+              >
+                <option
+                  v-for="item in channel_info"
+                  :key="item.id"
+                  :value="item.id"
+                >
+                  {{ item.name }}
+                </option>
               </select>
             </form>
           </div>
@@ -89,22 +91,16 @@
             <p style="margin-left: 0px">日期：</p>
 
             <!-- 引入eleui时间选择模块 -->
+            <!-- value-format="yyyy-MM-dd"eleui中更改绑定数据格式
+             format="yyyy-MM-dd"eleui中更改输入格式 -->
             <el-date-picker
               v-model="value1"
+              format="yyyy-MM-dd"
+              value-format="yyyy-MM-dd"
               type="datetimerange"
               start-placeholder="开始日期"
               end-placeholder="结束日期"
               :default-time="['12:00:00']"
-            >
-            </el-date-picker>
-            <p>至</p>
-            <el-date-picker
-              v-model="value2"
-              type="datetimerange"
-              align="right"
-              start-placeholder="开始日期"
-              end-placeholder="结束日期"
-              :default-time="['12:00:00', '08:00:00']"
             >
             </el-date-picker>
           </div>
@@ -117,88 +113,95 @@
 
     <!-- 目录部分 -->
     <div id="mulu">
-      <!-- 筛选结果总数 -->
-      <p>已经过滤筛选出{{ total_count }}件符合条件的文章</p>
+      <!-- 加载响应模块 -->
+      <div id="loading" v-show="is_load">
+        <p>loader..........</p>
+      </div>
+      <div id="show">
+        <!-- 筛选结果总数 -->
+        <p>已经过滤筛选出{{ total_count }}件符合条件的文章</p>
 
-      <!-- 表格展示部分 -->
-      <table>
-        <!-- 头部 -->
-        <thead>
-          <tr>
-            <td>图片</td>
-            <td>标题</td>
-            <td>状态</td>
-            <td>时间</td>
-            <td>操作</td>
-          </tr>
-        </thead>
-        <!-- 体 -->
-        <tbody>
-          <!-- 利用从接口获得的数据进行遍历 -->
-          <tr v-for="item in articles" :key="item.id" v-show="is_no_data">
-            <td>
-              <img
-                :src="item.cover.images[0]"
-                alt=""
-                id="img_r"
-                v-if="item.cover.images[0]"
-              />
+        <!-- 表格展示部分 -->
+        <table>
+          <!-- 头部 -->
+          <thead>
+            <tr>
+              <td>图片</td>
+              <td>标题</td>
+              <td>状态</td>
+              <td>时间</td>
+              <td>操作</td>
+            </tr>
+          </thead>
+          <!-- 体 -->
+          <tbody>
+            <!-- 利用从接口获得的数据进行遍历 -->
+            <tr v-for="item in articles" :key="item.id" v-show="is_no_data">
+              <td>
+                <img
+                  :src="item.cover.images[0]"
+                  alt=""
+                  id="img_r"
+                  v-if="item.cover.images[0]"
+                />
 
-              <!-- 图片文件----默认图片 -->
-              <img v-else src="./780.jpg" alt="" id="img_r" />
-            </td>
+                <!-- 图片文件----默认图片 -->
+                <img v-else src="./780.jpg" alt="" id="img_r" />
+              </td>
 
-            <!-- 文章标题 -->
-            <td class="wen_title">{{ item.title }}</td>
+              <!-- 文章标题 -->
+              <td class="wen_title">{{ item.title }}</td>
 
-            <!-- 处理文章审核状态的展示 -->
-            <td class="status">
-              <!-- 利用从接口处获得的文件状态码status来确定文章状态 -->
-              <div v-if="item.status == 0" class="caogao">草稿</div>
-              <div v-else-if="item.status == 3" class="sh_loging">审核中</div>
-              <div v-else-if="item.status == 1" class="sh_to">待审核</div>
-              <div v-else-if="item.status == 2" class="pass">通过审核</div>
-              <div v-else-if="item.status == 4" class="del">已删除</div>
-            </td>
+              <!-- 处理文章审核状态的展示 -->
+              <td class="status">
+                <!-- 利用从接口处获得的文件状态码status来确定文章状态 -->
+                <div v-if="item.status == 0" class="caogao">草稿</div>
+                <div v-else-if="item.status == 3" class="sh_loging">审核中</div>
+                <div v-else-if="item.status == 1" class="sh_to">待审核</div>
+                <div v-else-if="item.status == 2" class="pass">通过审核</div>
+                <div v-else-if="item.status == 4" class="del">已删除</div>
+              </td>
 
-            <!-- 文章发布时间 -->
-            <td>{{ item.pubdate }}</td>
+              <!-- 文章发布时间 -->
+              <td>{{ item.pubdate }}</td>
 
-            <!-- 文章的编辑删除模块 -->
-            <td>
-              <button id="edit_btn">编辑</button>
-              <button id="del_btn">删除</button>
-            </td>
-          </tr>
+              <!-- 文章的编辑删除模块 -->
+              <td>
+                <button id="edit_btn">编辑</button>
+                <button id="del_btn">删除</button>
+              </td>
+            </tr>
 
-          <tr v-show="!is_no_data">
-            <td>暂无数据</td>
-            <td>暂无数据</td>
-            <td>暂无数据</td>
-            <td>暂无数据</td>
-            <td>暂无数据</td>
-          </tr>
-        </tbody>
-      </table>
-
-      <!-- 模块底部跳转页功能----eleui -->
-      <!-- @current-change----当前页码发生改变时触发 -->
-      <!-- total_count文章总数 -->
-      <div id="yema">
-        <el-pagination
-          background
-          layout="prev, pager, next"
-          :total="pages_updata"
-          @current-change="yema"
-        >
-        </el-pagination>
+            <tr v-show="!is_no_data">
+              <td>暂无数据</td>
+              <td>暂无数据</td>
+              <td>暂无数据</td>
+              <td>暂无数据</td>
+              <td>暂无数据</td>
+            </tr>
+          </tbody>
+        </table>
+        <!-- 模块底部跳转页功能----eleui -->
+        <!-- @current-change----当前页码发生改变时触发 -->
+        <!-- total_count文章总数 -->
+        <!-- :disabled="is_load"请求期间禁止导航按钮 -->
+        <div id="yema">
+          <el-pagination
+            background
+            :disabled="is_load"
+            layout="prev, pager, next"
+            :total="pages_updata"
+            @current-change="yema"
+          >
+          </el-pagination>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { articles } from "../../apis/article";
+import { articles, channel, channel_id } from "../../apis/article";
 // 引入专门处理文章相关的请求方法
 export default {
   name: "const",
@@ -206,25 +209,41 @@ export default {
     return {
       // 文章信息------从接口方法中获得
       articles: [],
+
+      // 文章频道信息
+      channel_info: [],
+      // 文章筛选id状态
+      channl_status: undefined,
       // 时间参数
-      value1: "",
-      value2: "",
+      value1: [],
+
       // 当前页码
       time_li: 1,
+
       // 文章总页码
       pages_updata: 0,
+
       // 文章总数
       total_count: 0,
+
       // 控制有无文章是的展示
       is_no_data: "",
+
       // 表单提交的筛选数据状态要求
       from_status: null,
+
+      // loading模块控制
+      is_load: false,
     };
   },
 
   created() {
+    // 初始化加载table数据时会加载loading
+    this.is_load = true;
     // 在组件加载完成时触发请求方法
     this.articles_updata();
+    // 调用筛选频道信息
+    this.channel_data();
   },
 
   methods: {
@@ -234,12 +253,25 @@ export default {
       let tokens = localStorage.getItem("token");
 
       // 调用请求方法---将用户令牌传递-----将接口参数传递
+
       articles(tokens, {
-        page: this.time_li, //处理在第几页
-        per_page: 20, //处理一页包含多少数据
+        //处理在第几页
+        page: this.time_li,
+        //处理一页包含多少数据
+
+        per_page: 20,
+
+        // 筛选文章的频道内容
+        channel_id: this.channl_status,
 
         // 筛选的文章状态选项
         status: this.from_status,
+
+        // 文章时间筛选状态
+        // 开始----若是用户传递了任意的空时间,依然将其作为参数请求
+        begin_pubdate: this.value1 ? this.value1[0] : undefined,
+        // 结束
+        end_pubdate: this.value1 ? this.value1[1] : undefined,
       })
         .then((data) => {
           let datas = data.data.data;
@@ -252,34 +284,33 @@ export default {
 
           // 调用控制页码总数的方法
           this.page_sum(datas);
+
+          // 每当加载完成后取消loading
+          this.is_load = false;
         })
         .catch((err) => {
           console.log(err);
         });
     },
 
-    // 页码改变
-    yema(val) {
-      this.time_li = val;
-      this.articles_updata();
-    },
-
-    // 页码总数
-    page_sum(data) {
-      // 满20的页码数
-      let yema = parseInt(data.total_count / 20);
-      let i = 0;
-
-      // 判断页码在满与不满的情况下页码数
-      if (data.total_count % 20) {
-        i = i + 1;
-      }
-      this.pages_updata = (yema + i) * 10;
+    // 获取文章频道
+    channel_data() {
+      // 从本地获取用户令牌
+      let tokens = localStorage.getItem("token");
+      // 调用文章获取方法
+      channel(tokens).then((data) => {
+        let arr = data.data.data.channels;
+        // 向数据中添加一个一个自定义全部数据及id
+        // id为空时筛选全部频道数据
+        arr.push({ name: "全部", id: undefined });
+        // 交给vue来渲染
+        this.channel_info = arr;
+      });
     },
 
     // 文章总数
     sum(data) {
-      console.log(data.total_count);
+      // console.log(data.total_count);
 
       // 当文章数量为0时展示另一个数据;
       if (data.total_count == 0) {
@@ -292,9 +323,31 @@ export default {
       this.total_count = data.total_count;
     },
 
+    // 页码改变
+    yema(val) {
+      // 触发页码导航时加载loading
+      this.is_load = true;
+      // val为更改的当前页
+      this.time_li = val;
+      // 重新请求
+      this.articles_updata();
+    },
+
+    // 页码总数
+    page_sum(data) {
+      // 满20的页码数----不满取整
+      let yema = Math.ceil(data.total_count / 20);
+
+      //  pages_updata为显示的页数
+      this.pages_updata = yema * 10;
+    },
+
     // 提交筛选要求
     commit_btn() {
-      // 调用请求方法刷新数据
+      // 调用请求方法刷新数据-----当页面筛选信息更改时点击按钮触发刷新请求
+      // 当更改信息重新请求时加载loading
+      this.is_load = true;
+      //包括id更改,时间更改,状态更改
       this.articles_updata();
     },
   },
@@ -379,7 +432,7 @@ export default {
           #bottom_munes {
             width: 120px;
             height: 30px;
-            text-indent: 2em;
+            text-indent: 1em;
             margin-left: 10px;
           }
         }
@@ -412,118 +465,148 @@ export default {
 
   // mulu------展示区
   #mulu {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    overflow: auto;
+    position: relative;
     width: 90%;
-    height: 480px;
     background-color: #fff;
 
-    p {
-      height: 50px;
-      text-align: center;
-      line-height: 50px;
-      font-size: 30px;
-      font-weight: 700;
-      font-family: "KaiTi";
-      margin-left: 20px;
-    }
+    // loading
+    #loading {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.5);
 
-    // 展示表格
-    table {
-      width: 90%;
-      border: 1px solid #ccc;
-      border-collapse: collapse;
-      // 头
-      thead {
-        height: 40px;
-        background-color: #fff;
-        td {
-          // width: 100px;
-          text-align: center;
-        }
-      }
-      // 体
-      tbody {
-        width: 100%;
-        border-top: 1px solid #ccc;
-        // 状态栏样式
-        .status {
-          width: 120px;
-        }
-        // 标题样式
-        .wen_title {
-          width: 500px;
-          padding: 0px 20px;
-        }
-
-        td {
-          height: 180px;
-          text-align: center;
-          border: 1px solid #ccc;
-
-          // 文章审核状态样式
-          .caogao,
-          .sh_loging,
-          .pass,
-          .del,
-          .sh_to {
-            width: 100px;
-            height: 40px;
-            color: #fff;
-            line-height: 40px;
-            margin-left: 10px;
-            font-weight: 700;
-          }
-
-          .caogao {
-            background-color: yellow;
-          }
-
-          .pass {
-            background-color: rgb(126, 241, 116);
-          }
-
-          .sh_loging {
-            background-color: rgb(207, 178, 46);
-          }
-
-          .del {
-            background-color: red;
-          }
-          .sh_to {
-            background-color: rgb(74, 35, 216);
-          }
-
-          // 编辑-删除按钮样式
-          #edit_btn,
-          #del_btn {
-            width: 50px;
-            height: 50px;
-            outline: 0;
-            border: 0px;
-            color: #fff;
-            font-weight: 700;
-            border-radius: 50%;
-          }
-
-          #edit_btn {
-            background-color: skyblue;
-          }
-          #del_btn {
-            margin-left: 10px;
-            background-color: red;
-          }
-        }
+      p {
+        width: 500px;
+        height: 100px;
+        color: #fff;
+        font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+        font-weight: 700;
+        text-align: center;
+        line-height: 100px;
+        font-size: 50px;
       }
     }
 
-    // 跳转页码
-    #yema {
-      margin: 20px 0px;
+    // show展示区
+    #show {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      overflow: auto;
+      height: 480px;
+
+      p {
+        height: 50px;
+        text-align: center;
+        line-height: 50px;
+        font-size: 30px;
+        font-weight: 700;
+        font-family: "KaiTi";
+        margin-left: 20px;
+      }
+      // 展示表格
+      table {
+        width: 96%;
+        border: 1px solid #ccc;
+        border-collapse: collapse;
+        // 头
+        thead {
+          height: 40px;
+          background-color: #fff;
+          td {
+            // width: 100px;
+            text-align: center;
+          }
+        }
+        // 体
+        tbody {
+          width: 100%;
+          border-top: 1px solid #ccc;
+          // 状态栏样式
+          .status {
+            width: 120px;
+          }
+          // 标题样式
+          .wen_title {
+            width: 500px;
+            padding: 0px 20px;
+          }
+
+          td {
+            height: 180px;
+            text-align: center;
+            border: 1px solid #ccc;
+
+            // 文章审核状态样式
+            .caogao,
+            .sh_loging,
+            .pass,
+            .del,
+            .sh_to {
+              width: 100px;
+              height: 40px;
+              color: #fff;
+              line-height: 40px;
+              margin-left: 10px;
+              font-weight: 700;
+            }
+
+            .caogao {
+              background-color: yellow;
+            }
+
+            .pass {
+              background-color: rgb(126, 241, 116);
+            }
+
+            .sh_loging {
+              background-color: rgb(207, 178, 46);
+            }
+
+            .del {
+              background-color: red;
+            }
+            .sh_to {
+              background-color: rgb(74, 35, 216);
+            }
+
+            // 编辑-删除按钮样式
+            #edit_btn,
+            #del_btn {
+              width: 50px;
+              height: 50px;
+              outline: 0;
+              border: 0px;
+              color: #fff;
+              font-weight: 700;
+              border-radius: 50%;
+            }
+
+            #edit_btn {
+              background-color: skyblue;
+            }
+            #del_btn {
+              margin-left: 10px;
+              background-color: red;
+            }
+          }
+        }
+      }
+
+      // 跳转页码
+      #yema {
+        margin: 20px 0px;
+      }
     }
   }
+
   // 文章配图样式
   #img_r {
     width: 100px;
